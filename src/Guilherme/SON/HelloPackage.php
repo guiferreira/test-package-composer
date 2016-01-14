2b1d82fc69ea0a891260b7a31431770b17b2e0ba
Guilherme Ferreira <guilherme27set@gmail.com>
2016-01-14 15:12:42 -0200

    Comit Inicial

---
+++ src/Guilherme/SON/HelloPackage.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guilherme
+ * Date: 14/01/2016
+ * Time: 14:02
+ */
+
+namespace Guilherme\SON;
+
+
+class HelloPackage
+{
+    public function getHello()
+    {
+        return "Hello Package!";
+    }
+}
